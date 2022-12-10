163b515506bb065db6864cafd75808f09fd995f5
Sanan Eminli <57816597+sananeminli@users.noreply.github.com>
2022-12-10 23:48:09 +0300

    Create halloweenSale.java

---
+++ halloweenSale.java
@@ -0,0 +1,21 @@
+    
+    
+    
+    
+    
+    
+    public static int howManyGames(int price, int sale, int min_price, int capital) {
+            if (price>capital) return 0;
+            int res = 0;
+            res++;
+            capital-= price;
+            while(price-sale> min_price&&capital-price>0){
+                price-=sale;
+                capital-=price;
+                res++;
+            }
+            int remaining = capital/price;
+
+
+        return res+remaining;
+    }
